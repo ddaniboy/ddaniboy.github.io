8ae3ae0c2a920e3d203561a03187baca3fb2c1e4
DaniBoy <dandanflima@gmail.com>
2022-05-13 08:35:30 -0300

    animation bug fix

--- content/js/request.js
+++ content/js/request.js
@@ -24,8 +24,9 @@ function destroyDoc() {
     document.getElementById('resp').style.animation="documentDestroyAnimation 4s";
     document.getElementById('resp').style.right = "-200%";
     setTimeout(() => {document.getElementById("resp").innerHTML = ""}, 5000);
-    
+    setTimeout(() => {document.getElementById('resp').style = ""}, 5000);
 
     document.getElementById('box').style.animation="barAnimation 4s";
     document.getElementById('box').style.left = "0%";
+    document.getElementById('box').style = "";
 }

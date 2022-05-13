fafde44640183f5b7311f60dcc66e829a3814e7d
DaniBoy <dandanflima@gmail.com>
2022-05-13 06:34:13 -0300

    Animation

--- content/js/request.js
+++ content/js/request.js
@@ -9,4 +9,23 @@ function callDoc(page) {
     
     document.getElementById("resp").innerHTML = xhttp.responseText;
 
+    xhttp.abort();
+
+    document.getElementById('box').style.animation="barAnimationRetract 4s";
+    document.getElementById('box').style.left = "-40%";
+}
+
+
+
+
+
+
+function destroyDoc() {
+    document.getElementById('resp').style.animation="documentDestroyAnimation 4s";
+    document.getElementById('resp').style.right = "-200%";
+    setTimeout(() => {document.getElementById("resp").innerHTML = ""}, 5000);
+    
+
+    document.getElementById('box').style.animation="barAnimation 4s";
+    document.getElementById('box').style.left = "0%";
 }
